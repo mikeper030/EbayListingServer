7204fb0554f1ff5c481254afd9ca05f6fb1bc169
Mike Peretz <mikeper030@gmail.com>
2020-01-23 22:05:53 +0200

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/',function(req, res, next) {
-  
-  res.render('index', { title: 'Express' });
+  function scaryClown() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve('🤡');
+      }, 2000);
+    });
+  }
+
+  async function msg() {
+    const msg = await scaryClown();
+    res.render('index', { title: 'Express' });
+    console.log('Message:', msg);
+  }
+
+  msg(); 
+
 });
 
 module.exports = router;
