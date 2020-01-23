938620701b74d7e3713e66072fe54c00a5afad55
Mike Peretz <mikeper030@gmail.com>
2020-01-23 22:21:46 +0200

    Update get_product.js

--- routes/get_product.js
+++ routes/get_product.js
@@ -1,31 +1,53 @@
 var express = require('express');
 var router = express.Router();
 var finishLineScraper = require('../scrapers/finishline_scraper');
+const cheerio = require('cheerio');
+const axios = require('axios');
 
-router.post('/',async function(req, res, next) {
+router.post('/',function(req, res, next) {
     res.header('Content-Type: application/json');
     let url = req.body.url;
-    let provider = req.body.provider;
-    console.log(url);
-   
-    var result = {};
-    if(url==="undefined"||provider==="undefined"){
-        result={"Error":"missing request params"};
-        res.send(result);
-    }
-    if (provider==="finishline"){
-        res.send("ok");
-       try {
-           console.log("processing finishline")
-           result = await finishLineScraper.newRequest(url);
-     
-         res.type('json').send(JSON.stringify(result, null, 2) + '\n');
-       }catch (e) {
-            res.send({"error":e});
-           console.log(e)
-       }
 
+    console.log(url);
+    var result={
+        url:url,
+        img:"",
+        sizes:[]
+    };
+
+    try {
+        axios.get(url)
+            .then((response) => {
+                let $ =  cheerio.load(response.data);
+                $('#productSizes button').each(function (i,el) {
+                    var size={};
+                    size.size=($(el).text().trim());
+                    let classAttr=$(el).attr('class');
+                    size.available=!classAttr.includes('disabled');
+                    result.sizes.push(size);
+
+                    console.log($(el).attr('class'));
+                    console.log(($(el).text().trim()));
+                })
+                let style= $('#styleColors').children().eq(1).html().trim().replace("- ","");
+                let s =style.replace("&#xA0;","_");
+
+                let img="//images.finishline.com/is/image/FinishLine/"+s+"_P1?$default$&wid=671&bgc=eeeeee"
+                result.img=img;
+                res.type('json').send(JSON.stringify(result, null, 2) + '\n');
+
+
+
+            }).catch(function (e) {
+            console.log(e);
+
+        });
+    }catch (e) {
+        console.log(e);
+
+        res.send(e);
     }
+
 
 });
 
